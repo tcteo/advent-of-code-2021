7fc575b2326d1530fd6738c3910d2af4fcaa700b
TC Teo <tsechin@tsechin.com>
2021-12-01 16:58:16 +1100

    wrap code in main()

--- day01/day1part1.py
+++ day01/day1part1.py
@@ -1,12 +1,16 @@
-with open('input.txt', 'r') as f:
-  lines = f.readlines()
-  lines = [l.strip() for l in lines]
+def main():
+  with open('input.txt', 'r') as f:
+    lines = f.readlines()
+    lines = [l.strip() for l in lines]
 
-numbers = [int(x) for x in lines]
+  numbers = [int(x) for x in lines]
 
-increase_count = 0
-for i in range(1, len(numbers)):
-  if numbers[i] > numbers[i-1]:
-    increase_count += 1
+  increase_count = 0
+  for i in range(1, len(numbers)):
+    if numbers[i] > numbers[i-1]:
+      increase_count += 1
 
-print(increase_count)
+  print(increase_count)
+
+if __name__ == '__main__':
+  main()
--- day01/day1part2.py
+++ day01/day1part2.py
@@ -8,18 +8,23 @@ def windowed_sums(numbers, window_size=3):
       yield sum(numbers[i-window_size+1:i+1])
     i += 1
 
-with open('input.txt', 'r') as f:
-  lines = f.readlines()
-  lines = [l.strip() for l in lines]
+def main():
 
-numbers = [int(x) for x in lines]
+  with open('input.txt', 'r') as f:
+    lines = f.readlines()
+    lines = [l.strip() for l in lines]
 
-prev = None
-increase_count = 0
-for w in windowed_sums(numbers):
-  if prev is not None:
-    if w > prev:
-      increase_count += 1
-  prev = w
+  numbers = [int(x) for x in lines]
 
-print(increase_count)
+  prev = None
+  increase_count = 0
+  for w in windowed_sums(numbers):
+    if prev is not None:
+      if w > prev:
+        increase_count += 1
+    prev = w
+
+  print(increase_count)
+
+if __name__ == '__main__':
+  main()
