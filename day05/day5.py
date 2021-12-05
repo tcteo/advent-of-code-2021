471daa12dabb49b1314f8ceb49c764ea97a89db5
TC Teo <tsechin@tsechin.com>
2021-12-05 17:03:16 +1100

    day5part2

--- day05/day5.py
+++ day05/day5.py
@@ -39,16 +39,15 @@ def parse_vent_coords(input_filename):
 
 def coords_to_map(vent_coords):
   # determine map size
-  max_x = max([c[0].x for c in vent_coords] + [c[1].x for c in vent_coords])
+  max_x = max([c[0].x for c in vent_coords] + [c[1].x for c in vent_coords])+1
   print(f'{max_x=}')
-  max_y = max([c[0].y for c in vent_coords] + [c[1].y for c in vent_coords])
+  max_y = max([c[0].y for c in vent_coords] + [c[1].y for c in vent_coords])+1
   print(f'{max_y=}')
   grid = np.zeros((max_x, max_y), dtype=np.int32)
   
   for c in vent_coords:
-    # detect the direction of a line
     if c[0].x == c[1].x:
-      x_line = True
+      # x-line
       a = c[0].y
       b = c[1].y
       if a>b:
@@ -57,7 +56,7 @@ def coords_to_map(vent_coords):
         a=swap
       grid[c[0].x, a:b+1] += 1
     elif c[0].y == c[1].y:
-      x_line = False
+      # y-line
       a = c[0].x
       b = c[1].x
       if a>b:
@@ -65,9 +64,21 @@ def coords_to_map(vent_coords):
         b=a
         a=swap
       grid[a:b+1, c[0].y] += 1
+    elif abs(c[0].x-c[1].x) == abs(c[0].y-c[1].y):
+      linelen = abs(c[0].x-c[1].x)
+      step = (c[1].x-c[0].x, c[1].y-c[0].y)
+      step = (int(step[0]/abs(step[0])), int(step[1]/abs(step[1])))
+      print(step)
+      # step is one of (1,1), (1,-1), (-1,1), (-1,-1)
+      if step not in [(1,1), (1,-1), (-1,1), (-1,-1)]:
+        raise Exception(f'cant handle {step=} between {c[0]} and {c[1]}')
+      pos = c[0]
+      for _ in range(0,linelen+1):
+        grid[pos.x, pos.y] += 1
+        pos = Coord(pos.x+step[0], pos.y+step[1])
+
     else:
-      pass
-      #raise Exception(f'diagonal line from {c[0]} to {c[1]}')
+      raise Exception(f'unhandled line direction between {c[0]} and {c[1]}')
 
   return grid
 
