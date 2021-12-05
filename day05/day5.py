8d75d89dcc6d5a85c37ba219aac473bff1eda238
TC Teo <tsechin@tsechin.com>
2021-12-05 17:06:24 +1100

    bit of clean up

--- day05/day5.py
+++ day05/day5.py
@@ -4,11 +4,11 @@ import numpy as np
 
 def main():
   vent_coords = parse_vent_coords('input.txt')
-  print(vent_coords)
+  # print(vent_coords)
 
   grid = coords_to_map(vent_coords)
 
-  print(f'{np.sum(grid)=}')
+  # print(f'{np.sum(grid)=}')
 
   grid_min2 = grid>=2
   print(f'{np.sum(grid_min2)=}')
@@ -37,12 +37,18 @@ def parse_vent_coords(input_filename):
       vent_coords.append((c1, c2))
   return vent_coords
 
+def a_lt_b(a, b):
+  if a<=b:
+    return (a,b)
+  else:
+    return (b,a)
+
 def coords_to_map(vent_coords):
   # determine map size
   max_x = max([c[0].x for c in vent_coords] + [c[1].x for c in vent_coords])+1
-  print(f'{max_x=}')
+  # print(f'{max_x=}')
   max_y = max([c[0].y for c in vent_coords] + [c[1].y for c in vent_coords])+1
-  print(f'{max_y=}')
+  # print(f'{max_y=}')
   grid = np.zeros((max_x, max_y), dtype=np.int32)
   
   for c in vent_coords:
@@ -50,25 +56,18 @@ def coords_to_map(vent_coords):
       # x-line
       a = c[0].y
       b = c[1].y
-      if a>b:
-        swap=b
-        b=a
-        a=swap
+      a,b = a_lt_b(a,b)
       grid[c[0].x, a:b+1] += 1
     elif c[0].y == c[1].y:
       # y-line
       a = c[0].x
       b = c[1].x
-      if a>b:
-        swap=b
-        b=a
-        a=swap
+      a,b = a_lt_b(a,b)
       grid[a:b+1, c[0].y] += 1
     elif abs(c[0].x-c[1].x) == abs(c[0].y-c[1].y):
       linelen = abs(c[0].x-c[1].x)
       step = (c[1].x-c[0].x, c[1].y-c[0].y)
       step = (int(step[0]/abs(step[0])), int(step[1]/abs(step[1])))
-      print(step)
       # step is one of (1,1), (1,-1), (-1,1), (-1,-1)
       if step not in [(1,1), (1,-1), (-1,1), (-1,-1)]:
         raise Exception(f'cant handle {step=} between {c[0]} and {c[1]}')
