68270998f115a82d1f36774d3d859e3e7b26b546
TC Teo <tsechin@tsechin.com>
2021-12-09 21:19:36 +1100

    less output

--- day09/day9part2.py
+++ day09/day9part2.py
@@ -3,13 +3,13 @@ import numpy as np
 def main():
   grid = parse_input('input.txt')
   # grid = parse_input('test.txt')
-  print(f'{grid=}')
+  # print(f'{grid=}')
   mingrid = min_points(grid) # grid with True at minima
-  print(f'{mingrid=}')
+  # print(f'{mingrid=}')
 
   min_locations_where = np.where(mingrid==True) # two arrays, list of i's and list of j's
   min_locations = list(zip(*min_locations_where)) # a list of tuples [(i,j),(i,j),...]
-  print(f'{len(min_locations)=}')
+  # print(f'{len(min_locations)=}')
   basin_sizes = []
   for mi,mj in min_locations:
     bs = basin_size(grid, mi, mj)
@@ -29,7 +29,7 @@ def basin_size(grid, mi, mj):
   basin_loc = np.zeros(grid.shape, dtype=np.bool)
   basin_loc[mi,mj]=True # min point is part of the basin
   basin_size = np.sum(basin_loc)
-  print(f'basin starting at {mi},{mj}')
+  # print(f'basin starting at {mi},{mj}')
   while True:
     # try to expand the basin
     basin_positions = list(zip(*(np.where(basin_loc==True))))
@@ -42,12 +42,12 @@ def basin_size(grid, mi, mj):
         if basin_loc[i,j]==True: continue # already marked
         if grid[i,j]>=9: continue # limit of basin
         basin_loc[i,j] = True # set as part of the current basin
-        print(f'              add {i},{j}')
+        # print(f'              add {i},{j}')
     new_basin_size = np.sum(basin_loc)
     if new_basin_size == basin_size:
       break # no growth, we are done expanding the basin
     basin_size = new_basin_size
-  print(f'basin_loc:\n{basin_loc}')
+  # print(f'basin_loc:\n{basin_loc}')
   return basin_size
 
 def parse_input(infile):
